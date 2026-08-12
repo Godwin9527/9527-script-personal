c26febea148b39f4536341ed93699b2220b39b52
Godwin9527 <Godwin9527@users.noreply.github.com>
2026-08-13 02:59:18 +0800

    feat: 添加 B站9527助手 自用脚本(独立改造版)

---
+++ bilibili-9527-helper.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         B站9527助手 - 自动宽屏|自定义布局|智能连播|打造属于自己的B站
+// @namespace    https://github.com/Godwin9527
+// @version      1.0.0
+// @description  9527自用: 自动宽屏模式|自定义布局|智能连播...更多功能等你体验, 打造属于自己的B站~
+// @author       Godwin9527
+// @run-at       document-start
+// @match        *://*.bilibili.com/*
+// @icon         https://www.bilibili.com/favicon.ico
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @noframes
+// @license      MIT
+// @downloadURL https://raw.githubusercontent.com/Godwin9527/9527-script-personal/main/bilibili-9527-helper.user.js
+// @updateURL https://raw.githubusercontent.com/Godwin9527/9527-script-personal/main/bilibili-9527-helper.meta.js
+// ==/UserScript==
